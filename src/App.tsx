5bb262d6b449cacf158d71d28a11e8fc8827f0f1
rellyso <rellysondouglas2015@gmail.com>
2021-03-27 13:56:40 -0300

    refraathing to ts type

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Routes } from './routes';
+
+import GlobalStyle from './styles/global';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  )
+};
